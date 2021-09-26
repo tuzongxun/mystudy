0fe72a62306c61569549d677e1cfefd0b3faf2b3
tuzongxun <1160569243@qq.com>
2021-09-26 20:26:53 +0800

    add back code

---
+++ springboot-vue-back/src/main/java/com/tzx/springbootvueback/SpringbootVueBackApplication.java
@@ -0,0 +1,13 @@
+package com.tzx.springbootvueback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootVueBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootVueBackApplication.class, args);
+	}
+
+}
