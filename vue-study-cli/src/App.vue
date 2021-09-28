5f17b20c0426ef1e4a2235d35fcfef5d3f79dcb9
tuzongxun <1160569243@qq.com>
2021-09-28 22:48:01 +0800

    增加vue-cli基础工程

---
+++ vue-study-cli/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <p>{{num}}</p>
+    <button @click="add">加一</button>
+    <hr/>
+    <p>父组件，我的钱：{{money}}</p>
+    <p>父组件，我的车：{{car}}</p>
+    <hr/>
+    <son :mon="money" @gift="show($event)"></son>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import son from './components/Son';
+import Son from './components/Son.vue';
+
+export default {
+  name: 'App',
+  // components: {
+  //   HelloWorld
+  // }
+  data(){
+    return{
+      num:0,
+      car: "",
+      money:"我有一个亿"
+    };
+  },
+  methods:{
+    add(){
+      this.num++;
+    },
+    show($event){
+      this.car=$event;
+    }
+  },
+  components:{
+    son
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-study-cli/src/components/Son.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="son">
+        <p>儿子组件，我的钱：{{mon}}
+        </p>
+        <p>儿子组件,{{car}}</p>
+        <button @click="sendCar">给父组件</button>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            car:"奔驰"
+        }
+    },
+    methods:{
+        sendCar(){
+            this.$emit("gift",this.car);
+        }
+    },
+    props:["mon"]
+}
+</script>
+<style scoped>
+.son{
+    width:200px;
+    height: 200px;
+    background-color: yellow;
+}
+</style>
